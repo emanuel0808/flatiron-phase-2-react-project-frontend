132456316abecf9ae9174d9502a62f5da27131c7
Emanuel Gaona <gaonaemanuel7@gmail.com>
2023-09-13 15:12:47 -0700

    Added header.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import Header from './components/Header';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(7)
 
   return (
+    
     <>
+
+      <Header />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
